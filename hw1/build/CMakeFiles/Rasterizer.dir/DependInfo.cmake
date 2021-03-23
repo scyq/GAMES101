57c04ff1bfe3944787022bc2afeaa450be1d10b3
scyq <chenyuqing0429@foxmail.com>
2021-03-23 19:57:17 +0800

    repush

--- hw1/build/CMakeFiles/Rasterizer.dir/DependInfo.cmake
+++ hw1/build/CMakeFiles/Rasterizer.dir/DependInfo.cmake
@@ -4,14 +4,16 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/yuqingchen/GAMES101/hw1/Triangle.cpp" "/home/yuqingchen/GAMES101/hw1/build/CMakeFiles/Rasterizer.dir/Triangle.cpp.o"
-  "/home/yuqingchen/GAMES101/hw1/main.cpp" "/home/yuqingchen/GAMES101/hw1/build/CMakeFiles/Rasterizer.dir/main.cpp.o"
-  "/home/yuqingchen/GAMES101/hw1/rasterizer.cpp" "/home/yuqingchen/GAMES101/hw1/build/CMakeFiles/Rasterizer.dir/rasterizer.cpp.o"
+  "/home/cs18/GAMES101/hw1/Triangle.cpp" "/home/cs18/GAMES101/hw1/build/CMakeFiles/Rasterizer.dir/Triangle.cpp.o"
+  "/home/cs18/GAMES101/hw1/main.cpp" "/home/cs18/GAMES101/hw1/build/CMakeFiles/Rasterizer.dir/main.cpp.o"
+  "/home/cs18/GAMES101/hw1/rasterizer.cpp" "/home/cs18/GAMES101/hw1/build/CMakeFiles/Rasterizer.dir/rasterizer.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/include/opencv"
   )
 
 # Targets to which this target links.
--- hw1/build/CMakeFiles/Rasterizer.dir/cmake_clean.cmake
+++ hw1/build/CMakeFiles/Rasterizer.dir/cmake_clean.cmake
@@ -1,9 +1,9 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Rasterizer.dir/Triangle.cpp.o"
   "CMakeFiles/Rasterizer.dir/main.cpp.o"
   "CMakeFiles/Rasterizer.dir/rasterizer.cpp.o"
-  "Rasterizer"
+  "CMakeFiles/Rasterizer.dir/Triangle.cpp.o"
   "Rasterizer.pdb"
+  "Rasterizer"
 )
 
 # Per-language clean rules from dependency scanning.
